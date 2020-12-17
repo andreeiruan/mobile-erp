643b3e838918ec4c8035d3e798d21b71e49cec1c
Andrei Ruã de Avila Silva <andreicruz.silva8765@gmail.com>
2020-12-17 12:24:39 -0300

    Initial configure project

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
+    'react/prop-types': [0, { ignore: ['navigation'] }],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true, optionalDependencies: true, peerDependencies: true }],
+    'no-use-before-define': 'off',
+    'react/style-prop-object': 'off',
+  },
+};

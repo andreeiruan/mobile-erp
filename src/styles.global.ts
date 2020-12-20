58ffd2cee3b79efa768e3a72d13e7f64a5859990
Andrei Ruã de Avila Silva <andreicruz.silva8765@gmail.com>
2020-12-20 09:03:36 -0300

    Create styles global

---
+++ src/styles.global.ts
@@ -0,0 +1,10 @@
+const colors = {
+  primaryColor: '#0274B3',
+  secondaryColor: '#347497',
+  textPrimaryColors: '#fff',
+  borderColors: '#485554',
+  backgroundColor: '#F0F0F0',
+
+};
+
+export { colors };

67685343998116111709eac32767c6f6faad15a5
Andrei Ruã de Avila Silva <andreicruz.silva8765@gmail.com>
2020-12-29 10:25:00 -0300

    adjust global styles

--- src/styles.global.ts
+++ src/styles.global.ts
@@ -1,10 +1,19 @@
 const colors = {
-  primaryColor: '#0274B3',
-  secondaryColor: '#347497',
-  textPrimaryColors: '#fff',
-  borderColors: '#485554',
-  backgroundColor: '#F0F0F0',
+  backgroundLinear: ['#363645', '#30303e', '#292935', '#23232d'],
+  // primaryColorLinear: ['#b55fc6', '#965dcd', '#8d61dc', '#7b61e7'],
+  primaryColorLinear: ['#4BB3F8', '#33A4EF', '#2092DD', '#0676C0'],
+  secondaryColorLinear: ['#E0E0E0', '#F8F3F3', '#E0E0E0', '#E2E2E2'],
 
+  inputBackgroundColor: '#FFF',
+  primaryColor: '#7e61e4',
+  secondaryColor: '#347497',
+
+  primaryFontColor: '#b9b9be',
+  secondaryFontColor: '#1F1E1E',
+  highlightedFontColor: '#FEFEFE',
+
+  menuColor: '#393948',
+  menuActiveFontColor: '#0676C0',
 };
 
 export { colors };

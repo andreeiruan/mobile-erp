a110c22209e7e1c3de320a74421a8a299807ec0b
Andrei Ruã de Avila Silva <andreicruz.silva8765@gmail.com>
2020-12-29 13:55:05 -0300

    Add provisious button to sign out on screen settings

--- src/screens/Settings/index.tsx
+++ src/screens/Settings/index.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 
-import { Text } from 'react-native';
+import { Text, SafeAreaView, TouchableOpacity } from 'react-native';
+import useAuth from '../../hooks/useAuth';
 
-import { Container } from './styles';
-
-const Settings: React.FC = () => (
-  <Container>
-    <Text>Settings</Text>
-  </Container>
-);
+const Settings: React.FC = () => {
+  const { signOut } = useAuth();
+  return (
+    <SafeAreaView>
+      <TouchableOpacity
+        style={{
+          alignSelf: 'center',
+          width: '50%',
+          backgroundColor: '#333',
+          marginTop: '50%',
+          padding: 15,
+          borderRadius: 8,
+        }}
+        onPress={signOut}
+      >
+        <Text style={{ color: '#fff', fontSize: 20, textAlign: 'center' }}>
+          Sair
+        </Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
 
 export default Settings;
